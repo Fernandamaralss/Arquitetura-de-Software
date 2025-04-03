47655202b2ac94ae942ecd0eeb0f34afa61b87fc
Fernanda Amaral <fernanda.amaralss@outlook.com>
2025-04-03 09:29:15 -0300

    Delete API-Simples-MongoDB02 directory

--- API-Simples-MongoDB02/server.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./src/app.js";
-
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Express server running in: http://localhost:${port}`);
-})
